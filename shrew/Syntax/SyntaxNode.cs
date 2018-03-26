5cbf13ba370fa44031cab315fd34dfbeff0344a3
phillyai <sonagi.philly@gmail.com>
2018-03-26 20:10:04 +0900

    신택스 노드 (AST) 전부 추가

--- shrew/Syntax/SyntaxNode.cs
+++ shrew/Syntax/SyntaxNode.cs
@@ -2,6 +2,19 @@
 {
     public class SyntaxNode
     {
+
+    }
+
+    public class AssignNode : SyntaxNode
+    {
+        public IdentifierNode Left;
+        public ExprNode Right;
+
+        public AssignNode(IdentifierNode left, ExprNode right)
+        {
+            Left = left;
+            Right = right;
+        }
     }
 
     public class ExprNode : SyntaxNode
@@ -11,6 +24,40 @@
 
     public class BinaryExprNode : ExprNode
     {
+        public ExprNode Left, Right;
+        public SyntaxToken Operator;
 
+        public BinaryExprNode(ExprNode left, ExprNode right, SyntaxToken op)
+        {
+            Left = left;
+            Right = right;
+            Operator = op;
+        }
+    }
+
+    public class TokenNode : ExprNode
+    {
+        public SyntaxToken Token;
+
+        public TokenNode(SyntaxToken identifier)
+        {
+            Token = identifier;
+        }
+    }
+
+    public class LiteralNode : TokenNode
+    {
+        public LiteralNode(SyntaxToken token) : base(token)
+        {
+
+        }
+    }
+
+    public class IdentifierNode : TokenNode
+    {
+        public IdentifierNode(SyntaxToken token) : base(token)
+        {
+
+        }
     }
 }
