6af8b07334523cb676391a0cbce6626a08d1f46a
phillyai <sonagi.philly@gmail.com>
2018-03-27 14:12:06 +0900

    파서 준비

---
+++ shrew/Parsing/Parser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using shrew.Lexing;
+using shrew.Syntax;
+
+namespace shrew.Parsing
+{
+    public class Parser
+    {
+        private Lexer _lexer;
+        public Parser(Lexer lexer)
+        {
+            _lexer = lexer;
+        }
+
+        public static SyntaxNode Parse(string code)
+        {
+            return new Parser(new Lexer(code)).Parse();
+        }
+
+        protected SyntaxNode Parse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
