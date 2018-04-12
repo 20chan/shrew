7ee3abd839c97023ae52049f74ca0c8f797dcc45
phillyai <sonagi.philly@gmail.com>
2018-04-12 11:21:03 +0900

    토큰 타입 확장메서드 추가

---
+++ shrew/Syntax/SyntaxTokenTypeExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace shrew.Syntax
+{
+    public static class SyntaxTokenTypeExtension
+    {
+        public static bool IsLiteral(this SyntaxTokenType type)
+        {
+            switch (type)
+            {
+                case SyntaxTokenType.IntegerLiteral:
+                case SyntaxTokenType.RealLiteral:
+                case SyntaxTokenType.StringLiteral:
+                case SyntaxTokenType.TrueKeyword:
+                case SyntaxTokenType.FalseKeyword:
+                case SyntaxTokenType.WildcardKeyword:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
